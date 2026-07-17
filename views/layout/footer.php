de838466ff5d2373ab7de9e2df7b554f7b489ad2
JarethHerrera <jherrera50977@ufide.ac.cr>
2026-07-17 16:59:15 -0600

    Update footer y header
    Update footer y header agregado

---
+++ views/layout/footer.php
@@ -0,0 +1,6 @@
+<!-- footer compartido entre todas las paginas -->
+<footer class="bg-light text-center py-4">
+  <p>Academia Codice</p>
+  <p>Instagram . X</p>
+  <p>(c) 2026 Academia Codice</p>
+</footer>
---
+++ views/layout/header.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="index.php?controller=index&action=index">Academia Codice</a>
+    <div class="navbar-nav">
+      <a class="nav-link" href="index.php?controller=index&action=index">Inicio</a>
+      <a class="nav-link" href="index.php?controller=cursos&action=index">Cursos</a>
+      <a class="nav-link" href="index.php?controller=profesores&action=index">Profesores</a>
+      <a class="nav-link" href="index.php?controller=contacto&action=index">Contacto</a>
+    </div>
+  </div>
+</nav>
